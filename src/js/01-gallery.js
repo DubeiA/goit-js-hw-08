18e96f6e4e9c2f3c5ea8bb406aa4767b81eb5eb4
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-12 11:31:05 +0300

    connect a simpleLightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const simple = require('simplelightbox')
+console.log(simple);
 // Change code below this line
 
 console.log(galleryItems);
