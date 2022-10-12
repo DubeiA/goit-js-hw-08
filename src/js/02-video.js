064d61a7e547fa5d78e43dfc73765e90811e30cd
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-12 17:37:14 +0300

    done task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+// console.log(Player);
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+const onPlay = function(data) {
+        duration: 61.857
+        percent: 1.049
+        seconds: 3.034
+      console.log("Оновився час");
+   
+    
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+
+    
+};
+
+const saveTime = localStorage.getItem("videoplayer-current-time");
+const parseTime = JSON.parse(saveTime)
+
+console.log(saveTime);
+
+ player.setCurrentTime(parseTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(onPlay , 1000));
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,77 @@
+import throttle from 'lodash.throttle'
+
+const FEADBACK_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = { 
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener("input", throttle(formDataInput, 500))
+
+
+// створює обєкт input та textarea і переробляє в JSON
+function formDataInput(evt) { 
+
+    formData[evt.target.name] = evt.target.value;
+
+    const formDataJSON = JSON.stringify(formData);
+    console.log(formDataJSON);
+
+    localStorage.setItem(FEADBACK_KEY , formDataJSON)
+}
+    
+
+populateTextarea() 
+
+populateInput ()
+
+// Відправлення форми (Безперезагрузки + очищення)
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    const readLocalStorage = localStorage.getItem(FEADBACK_KEY);
+    const showLocalStorage = JSON.parse(readLocalStorage);
+    console.log(showLocalStorage);
+
+    evt.currentTarget.reset()
+    localStorage.removeItem(FEADBACK_KEY)
+}
+// Виводить у локальне сховище текст , що вводиться
+function onTextareaInput(evt) {
+    const message = evt.target.value
+    
+    localStorage.setItem(FEADBACK_KEY, message);
+    
+}
+
+// після перезагрузки сторінки , залишає текст введений у поле textarea
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(FEADBACK_KEY);
+    const savedMessageParce = JSON.parse(savedMessage);
+
+    if (savedMessageParce) { 
+        console.log(savedMessageParce);
+        refs.textarea.value = savedMessageParce.message;
+        console.log(refs.textarea.value);
+    }
+
+}
+// після перезагрузки сторінки , залишає текст введений у поле email
+function populateInput() {
+    const savedEmailMessage = localStorage.getItem(FEADBACK_KEY);
+    const savedEmailParce = JSON.parse(savedEmailMessage);
+
+    if (savedEmailParce) { 
+        console.log(savedEmailParce);
+        refs.form.email.value = savedEmailParce.email;
+        console.log(refs.form.email.value);
+    }
+
+}
+
+
 
