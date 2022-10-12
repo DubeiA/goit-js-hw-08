1f83a073341ef2242bc9979e2dcd984ac7482761
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-12 11:34:13 +0300

    done task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const simple = require('simplelightbox')
-console.log(simple);
+
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGallery(images) {
+    return images
+        .map(image => `<a class="gallery__item" href="${image.original}">
+                         <img class="gallery__image"
+                          src=${image.preview}
+                          alt=${image.description} />
+                       </a>`)
+        .join('');
+}
+
+const addGallery = createGallery(galleryItems);
+
+const divEl = document.querySelector('.gallery')
+
+divEl.insertAdjacentHTML('beforeend', addGallery);
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
