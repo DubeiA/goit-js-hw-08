60c48f68a186bddfd322a8d7f14713d43c6b7612
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2022-10-21 14:40:20 +0300

    Update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,110 +1,52 @@
-// import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle'
 
-// const FEADBACK_KEY = 'feedback-form-state';
+const FEADBACK_KEY = 'feedback-form-state';
 
-// const formData = {};
 
-// const refs = { 
-//     form: document.querySelector(".feedback-form"),
-//     textarea: document.querySelector(".feedback-form textarea"),
+const refs = { 
+    form: document.querySelector(".feedback-form"),
+  textarea: document.querySelector(".feedback-form textarea"),
+    emailRef: document.querySelector('input'),
     
-// }
+}
 
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.form.addEventListener("input", throttle(formDataInput, 500))
-
-
-// // створює обєкт input та textarea і переробляє в JSON
-// function formDataInput(evt) { 
-
-//     formData[evt.target.name] = evt.target.value;
-
-//     const formDataJSON = JSON.stringify(formData);
-//     console.log(formDataJSON);
-
-//     localStorage.setItem(FEADBACK_KEY , formDataJSON)
-// }
-    
-
-// populateTextarea() 
-
-// populateInput ()
-
-// // Відправлення форми (Безперезагрузки + очищення)
-// function onFormSubmit(evt) { 
-//     evt.preventDefault();
-//     const readLocalStorage = localStorage.getItem(FEADBACK_KEY);
-//     const showLocalStorage = JSON.parse(readLocalStorage);
-//     console.log(showLocalStorage);
-
-//     evt.currentTarget.reset()
-//     localStorage.removeItem(FEADBACK_KEY)
-// }
-// // Виводить у локальне сховище текст , що вводиться
-// function onTextareaInput(evt) {
-//     const message = evt.target.value
-    
-//     localStorage.setItem(FEADBACK_KEY, message);
-    
-// }
-
-// // після перезагрузки сторінки , залишає текст введений у поле textarea
-// function populateTextarea() {
-//     const savedMessage = localStorage.getItem(FEADBACK_KEY);
-//     const savedMessageParce = JSON.parse(savedMessage);
-
-//     if (savedMessageParce) { 
-//         console.log(savedMessageParce);
-//         refs.textarea.value = savedMessageParce.message;
-//         console.log(refs.textarea.value);
-//     }
-
-// }
-// // після перезагрузки сторінки , залишає текст введений у поле email
-// function populateInput() {
-//     const savedEmailMessage = localStorage.getItem(FEADBACK_KEY);
-//     const savedEmailParce = JSON.parse(savedEmailMessage);
-
-//     if (savedEmailParce) { 
-//         console.log(savedEmailParce);
-//         refs.form.email.value = savedEmailParce.email;
-//         console.log(refs.form.email.value);
-//     }
-
-// }
-
-// ======================================================================
-
-var throttle = require('lodash.throttle');
-
-const formRef = document.querySelector('.feedback-form');
-const emailRef = document.querySelector('input');
-const messageRef = document.querySelector('textarea');
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener("input", throttle(formDataInput, 500))
 
 
-formRef.addEventListener('submit', function (event) {
-  event.preventDefault();
-  console.log({email: emailRef.value, message: messageRef.value});
-  event.currentTarget.reset();
-  localStorage.clear();
-})
-formRef.addEventListener('input', throttle(onForm, 500));
 
-function onForm(event) {
+// Відправлення форми (Безперезагрузки + очищення)
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    console.log({email: refs.emailRef.value, message: refs.textarea.value});
 
-  // const emailValue = event.target.value;
-  // const messageValue = event.target.value;
+  evt.currentTarget.reset()
+  
+    localStorage.clear()
+}
+
+// створює обєкт input та textarea і переробляє в JSON
+function formDataInput(evt) {
   
   const formData = {
-    email: formRef.elements.email.value,
-    message: formRef.elements.message.value,
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
   };
-    // formData.email = emailValue;
-    // formData.message = messageValue;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
   
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
+    
+
+// Виводить у локальне сховище текст , що вводиться
+function onTextareaInput(evt) {
+    const message = evt.target.value
+    
+    localStorage.setItem(FEADBACK_KEY, message);
+    
+}
+
+// після перезагрузки сторінки , залишає текст введений у поле textarea
 function getCurrentValuesOnForm() {
   const localStorageData = localStorage.getItem('feedback-form-state');
   if (localStorageData) {
@@ -114,4 +56,36 @@ function getCurrentValuesOnForm() {
 }
 };
 getCurrentValuesOnForm();
+
+
+// ======================================================================
+
+// const formRef = document.querySelector('.feedback-form');
+// const emailRef = document.querySelector('input');
+// const messageRef = document.querySelector('textarea');
+
+
+// formRef.addEventListener('submit', function (event) {
+//   event.preventDefault();
+//   console.log({email: emailRef.value, message: messageRef.value});
+//   event.currentTarget.reset();
+//   localStorage.clear();
+// })
+// formRef.addEventListener('input', throttle(onForm, 500));
+
+// function onForm(event) {
+
+//   // const emailValue = event.target.value;
+//   // const messageValue = event.target.value;
+  
+//   const formData = {
+//     email: formRef.elements.email.value,
+//     message: formRef.elements.message.value,
+//   };
+//     // formData.email = emailValue;
+//     // formData.message = messageValue;
+//     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  
+// }
+
 
